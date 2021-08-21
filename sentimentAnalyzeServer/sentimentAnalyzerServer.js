b1a525159492112911c4b7e3bb65831d798f241c
AV-Master73 <85580482+AV-Master73@users.noreply.github.com>
2021-08-21 18:44:01 +0300

    Update sentimentAnalyzerServer.js
    Try to make server endpoints

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,33 +1,128 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = new express();
+dotenv.config();
+
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url,
+    })
+    return naturalLanguageUnderstanding;
+}
 
 app.use(express.static('client'))
 
 const cors_app = require('cors');
 app.use(cors_app());
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render('index.html');
-  });
-
-app.get("/url/emotion", (req,res) => {
-
-    return res.send({"happy":"90","sad":"10"});
 });
 
-app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+app.get("/url/emotion", (req, res) => {
+    let resp = req.query.url + 'init/url/emotion';
+    const analyzeParams = {
+        'url': req.query.url,
+        'features': {
+            'emotion': {
+                'targets': [
+                    'humans',
+                    'robots'
+                ]
+            }
+        }
+    };
+    const analyzer = getNLUInstance();
+    analyzer.analyze(analyzeParams)
+        .then(analysisResults => {
+            resp = JSON.stringify(analysisResults, null, 2);
+        })
+        .catch(err => {
+            resp = ('error:' + err);
+        });
+    return res.send({ "happy": "73", "sad": "27", resp });
 });
 
-app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+app.get("/url/sentiment", (req, res) => {
+    let resp = req.query.url + 'init/url/sentiment';
+    const analyzeParams = {
+        'url': req.query.url,
+        'features': {
+            'sentiment': {
+                'targets': [
+                    'bonds'
+                ]
+            }
+        }
+    };
+    const analyzer = getNLUInstance();
+    analyzer.analyze(analyzeParams)
+        .then(analysisResults => {
+            resp = JSON.stringify(analysisResults, null, 2);
+        })
+        .catch(err => {
+            resp = ('error:' + err);
+        });
+    return res.send(resp);
 });
 
-app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+app.get("/text/emotion", (req, res) => {
+    let resp = req.query.text + 'init/text/emotion';
+    const analyzeParams = {
+        'text': req.query.text,
+        'features': {
+            'emotion': {
+                'targets': [
+                    'humans',
+                    'robots'
+                ]
+            }
+        }
+    };
+    const analyzer = getNLUInstance();
+    analyzer.analyze(analyzeParams)
+        .then(analysisResults => {
+            resp = JSON.stringify(analysisResults, null, 2);
+        })
+        .catch(err => {
+            resp = ('error:' + err);
+        });
+    return res.send({ "happy": "82", "sad": "18", resp });
 });
 
-let server = app.listen(8080, () => {
+app.get("/text/sentiment", (req, res) => {
+    let resp = req.query.text + 'init/text/sentiment';
+    const analyzeParams = {
+        'text': req.query.text,
+        'features': {
+            'sentiment': {
+                'targets': [
+                    'bonds'
+                ]
+            }
+        }
+    };
+    const analyzer = getNLUInstance();
+    analyzer.analyze(analyzeParams)
+        .then(analysisResults => {
+            resp = analysisResults;
+        })
+        .catch(err => {
+            resp = ('error:' + err);
+        });
+    return res.send(resp);
+});
+
+let server = app.listen(8090, () => {
     console.log('Listening', server.address().port)
 })
-
